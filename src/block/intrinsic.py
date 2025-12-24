5d381d590d074efd86a2968b0aa761443cb44cca
WenchaoHuang <huangwenchao@linctex.com>
2025-12-24 14:09:09 +0800

    Add intrinsic functions

---
+++ src/block/intrinsic.py
@@ -0,0 +1,169 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2025, Style3D
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import warp as wp
+
+########################################################################################################################
+#############################################    Native CUDA intrinsics    #############################################
+########################################################################################################################
+
+@wp.func_native("return gridDim.x;")
+def grid_dim() -> int:
+    """ Returns the number of blocks in the grid (1D).
+
+    Equivalent to CUDA's `gridDim.x`.
+
+    Note:
+        Warp kernels only support 1D launch configuration, so only the x-dimension is meaningful (y = z = 1).
+    """
+    ...
+
+
+@wp.func_native("return blockDim.x;")
+def block_dim() -> int:
+    """ Returns the number of threads per block (1D).
+
+    Equivalent to CUDA's `blockDim.x`.
+
+    Note:
+        Warp kernels only support 1D launch configuration, so only the x-dimension is meaningful (y = z = 1).
+    """
+    ...
+
+
+@wp.func_native("return blockIdx.x;")
+def block_id() -> int:
+    """ Returns the block index within the grid (1D).
+
+    Equivalent to CUDA's `blockIdx.x`.
+
+    Note:
+        Warp kernels only support 1D launch configuration, so only the x-component is meaningful (y = z = 0).
+    """
+    ...
+
+
+@wp.func_native("return threadIdx.x;")
+def thread_id() -> int:
+    """ Returns the thread index within its block (1D).
+
+    Equivalent to CUDA's `threadIdx.x`.
+
+    Note:
+        Warp kernels only support 1D launch configuration, so only the x-component is meaningful (y = z = 0).
+    """
+    ...
+
+
+@wp.func_native("""unsigned int laneId; asm("mov.u32 %0, %%warpid;" : "=r"(laneId)); return laneId;""")
+def warp_id() -> int:
+    """ Returns the SM warp scheduler slot ID of the calling thread (from %warpid).
+    """
+    ...
+
+
+@wp.func_native("""unsigned int laneId; asm("mov.u32 %0, %%laneid;" : "=r"(laneId)); return laneId;""")
+def lane_id() -> int:
+    """ Returns the thread's lane ID within its warp.
+
+    The lane ID is the thread index within a warp (range [0, 31]).
+    This value can be used for warp-level programming, e.g., warp shuffles,
+    prefix sums, or ballot operations.
+
+    Equivalent to CUDA's:
+        unsigned int laneId;
+        asm("mov.u32 %0, %%laneid;" : "=r"(laneId));
+    """
+    ...
+
+
+@wp.func_native("""return __clz(x);""")
+def clz(x: wp.uint32) -> int:
+    """ Count Leading Zeros (CLZ).
+        Returns the number of leading 0-bits in the 32-bit unsigned integer `x`.
+        If `x` is 0, the result is 32.
+        Equivalent to CUDA's `__clz()`.
+    """
+    ...
+
+
+@wp.func_native("""return __clz((unsigned int)x);""")
+def clz(x: wp.int32) -> int:
+    """ Count Leading Zeros (CLZ).
+        Returns the number of leading 0-bits in the 32-bit signed integer `x`.
+        If `x` is 0, the result is 32.
+        Equivalent to CUDA's `__clz()`.
+    """
+    ...
+
+
+@wp.func_native("""__threadfence();""")
+def threadfence():
+    """ Device-wide memory fence.
+        Ensures that all global and shared memory writes made by the calling thread
+        are visible to all threads on the device before any subsequent writes.
+        Equivalent to CUDA's `__threadfence()`.
+    """
+    ...
+
+
+@wp.func_native("""return __shfl_xor_sync(mask, var, lane_mask, width);""")
+def shfl_xor_sync(mask: int, var: int, lane_mask: int, width: int = 32) -> int:
+    """
+    Warp-level shuffle XOR operation (CUDA intrinsic wrapper) for integers.
+
+    This function allows threads within a warp to exchange values directly
+    through registers without using shared memory.
+    """
+    ...
+
+
+@wp.func_native("""return __shfl_xor_sync(mask, var, lane_mask, width);""")
+def shfl_xor_sync(mask: int, var: float, lane_mask: int, width: int = 32) -> float:
+    """
+    Warp-level shuffle XOR operation (CUDA intrinsic wrapper) for float-points.
+
+    This function allows threads within a warp to exchange values directly
+    through registers without using shared memory.
+    """
+    ...
+
+
+@wp.func
+def shfl_xor_sync(mask: int, var: wp.vec3, lane_mask: int, width: int = 32) -> wp.vec3:
+    """
+    Warp-level shuffle XOR operation (CUDA intrinsic wrapper) for 3D vectors.
+
+    This function allows threads within a warp to exchange values directly
+    through registers without using shared memory.
+    """
+    var.x = shfl_xor_sync(mask, var.x, lane_mask, width)
+    var.y = shfl_xor_sync(mask, var.y, lane_mask, width)
+    var.z = shfl_xor_sync(mask, var.z, lane_mask, width)
+    return var
---
+++ tests/test_intrinsic.py
@@ -0,0 +1,190 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2025, Style3D
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import warp as wp
+import block as bk
+
+########################################################################################################################
+####################################################    test_clz    ####################################################
+########################################################################################################################
+
+def test_clz():
+	
+	@wp.kernel
+	def test_clz_kernel(
+		ret_int: wp.array(dtype = int),
+		ret_uint: wp.array(dtype = int),
+	):
+		tid = wp.tid()
+		ret_int[tid] = bk.clz(wp.int32(tid))
+		ret_uint[tid] = bk.clz(wp.uint32(tid))
+	
+	dim = 128
+	ret_int = wp.zeros(dim, dtype=int)
+	ret_uint = wp.zeros(dim, dtype=int)
+	
+	wp.launch(
+		test_clz_kernel,
+		outputs = [ret_int, ret_uint],
+		dim = dim,
+	)
+	
+	ret_int_np = ret_int.numpy()
+	ret_uint_np = ret_uint.numpy()
+	
+	# Verify result
+	for i in range(dim):
+		val = i
+		ret = 32
+		while val != 0:
+			ret = ret - 1
+			val = val >> 1
+		assert ret_int_np[i] == ret_uint_np[i]
+		assert ret == ret_int_np[i]
+
+########################################################################################################################
+#################################################    test_shfl_xor    ##################################################
+########################################################################################################################
+
+def test_shfl_xor():
+	
+	@wp.kernel
+	def test_shfl_xor_kernel(
+		ret_int: wp.array(dtype = int),
+		ret_float: wp.array(dtype = float),
+		ret_vec3: wp.array(dtype = wp.vec3),
+	):
+		tid = wp.tid()
+		
+		var_int = int(tid)
+		var_int = var_int + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_int, lane_mask = 16)
+		var_int = var_int + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_int, lane_mask = 8)
+		var_int = var_int + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_int, lane_mask = 4)
+		var_int = var_int + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_int, lane_mask = 2)
+		var_int = var_int + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_int, lane_mask = 1)
+		
+		var_float = float(tid)
+		var_float = var_float + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_float, lane_mask = 16)
+		var_float = var_float + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_float, lane_mask = 8)
+		var_float = var_float + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_float, lane_mask = 4)
+		var_float = var_float + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_float, lane_mask = 2)
+		var_float = var_float + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_float, lane_mask = 1)
+		
+		var_vec3 = wp.vec3(wp.float32(tid))
+		var_vec3 = var_vec3 + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_vec3, lane_mask = 16)
+		var_vec3 = var_vec3 + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_vec3, lane_mask = 8)
+		var_vec3 = var_vec3 + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_vec3, lane_mask = 4)
+		var_vec3 = var_vec3 + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_vec3, lane_mask = 2)
+		var_vec3 = var_vec3 + bk.shfl_xor_sync(mask = 0xFFFFFFFF, var = var_vec3, lane_mask = 1)
+		
+		ret_int[tid] = var_int
+		ret_vec3[tid] = var_vec3
+		ret_float[tid] = var_float
+	
+	dim = 32
+	ret_int = wp.zeros(dim, dtype = int)
+	ret_float = wp.zeros(dim, dtype = float)
+	ret_vec3 = wp.zeros(dim, dtype = wp.vec3)
+	
+	wp.launch(
+		test_shfl_xor_kernel,
+		outputs = [ret_int, ret_float, ret_vec3],
+		dim = dim,
+	)
+	
+	ret_int_np = ret_int.numpy()
+	ret_vec3_np = ret_vec3.numpy()
+	ret_float_np = ret_float.numpy()
+	
+	ret = (0 + 31) * dim // 2
+	for i in range(dim):
+		assert ret_int_np[i] == ret
+		assert ret_float_np[i] == float(ret)
+		assert ret_vec3_np[i][0] == float(ret)
+		assert ret_vec3_np[i][1] == float(ret)
+		assert ret_vec3_np[i][2] == float(ret)
+		
+########################################################################################################################
+###############################################    test_launch_config    ###############################################
+########################################################################################################################
+
+def test_launch_config():
+	
+	@wp.kernel
+	def test_launch_config_kernel(
+		ret_warp_id: wp.array(dtype = int),
+		ret_lane_id: wp.array(dtype = int),
+		ret_thread_id: wp.array(dtype = int),
+		ret_block_id: wp.array(dtype = int),
+		ret_block_dim: wp.array(dtype = int),
+		ret_grid_dim: wp.array(dtype = int),
+	):
+		tid = wp.tid()
+		ret_warp_id[tid] = bk.warp_id()
+		ret_lane_id[tid] = bk.lane_id()
+		ret_thread_id[tid] = bk.thread_id()
+		ret_block_id[tid] = bk.block_id()
+		ret_block_dim[tid] = bk.block_dim()
+		ret_grid_dim[tid] = bk.grid_dim()
+	
+	dim = 128
+	ret_warp_id = wp.zeros(dim, dtype = int)
+	ret_lane_id = wp.zeros(dim, dtype = int)
+	ret_thread_id = wp.zeros(dim, dtype = int)
+	ret_block_id = wp.zeros(dim, dtype = int)
+	ret_block_dim = wp.zeros(dim, dtype = int)
+	ret_grid_dim = wp.zeros(dim, dtype = int)
+	
+	wp.launch(
+		test_launch_config_kernel,
+		outputs=[
+			ret_warp_id,
+			ret_lane_id,
+			ret_thread_id,
+			ret_block_id,
+			ret_block_dim,
+			ret_grid_dim,
+		],
+		block_dim = 64,
+		dim=dim,
+	)
+	
+	ret_warp_id_np = ret_warp_id.numpy()
+	ret_lane_id_np = ret_lane_id.numpy()
+	ret_thread_id_np = ret_thread_id.numpy()
+	ret_block_id_np = ret_block_id.numpy()
+	ret_block_dim_np = ret_block_dim.numpy()
+	ret_grid_dim_np = ret_grid_dim.numpy()
+	
+	for i in range(dim):
+		assert ret_thread_id_np[i] < 64				# random
+		assert ret_lane_id_np[i] == ret_thread_id_np[i] % 32
+		assert ret_block_id_np[i] < dim // 64		# random
+		assert ret_grid_dim_np[i] == dim // 64
+		assert ret_block_dim_np[i] == 64
